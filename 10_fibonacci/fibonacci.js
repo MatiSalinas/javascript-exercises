6d43f5eb9da67ebf321c3f04d976e1618017d20d
Matias Salinas <matixxdpls@gmail.com>
2022-09-21 16:09:14 -0300

    Added solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let num = parseInt(a);
+    if(num < 0){
+        return 'OOPS'
+    }
+    sequence = [1,1];
+    if(num == 1 || num == 2){
+        return sequence[num-1]
+    }else{
+        for(let i = 2 ; i<=num;i++){
+            fiboNum = sequence[i-1] + sequence [i-2];
+            sequence.push(fiboNum)
+        }
+        return sequence[num-1];
+    }
+    
 
 };
 
