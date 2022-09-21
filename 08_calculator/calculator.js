e16e00a60cd3d96bfca8e80ae5a07d4949791f82
Matias Salinas <matixxdpls@gmail.com>
2022-09-21 15:50:35 -0300

    Added Solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+
+  addition = a + b;
+  return addition;
 	
 };
 
-const subtract = function() {
+const subtract = function(a ,b) {
+
+	return (a-b);
+};
+
+const sum = function(arr) {
+  const sum = arr.reduce((total,num)=>{
+    return total + num;
+  },0)
+	return sum;
+};
+
+const multiply = function(arr) {
+  const product = arr.reduce((total,num)=>{
+    return total * num;
+  },1)
+	return product;
+
+};
+
+const power = function(a,b) {
+  return (a**b);
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(num) {
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  let total = 1;
+  for(let i = 1 ; i <= num; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const newStr = str.toLowerCase().replace(/[\W_]+/g,"");
 
+    let j = newStr.length - 1;
+    let palin = false
+    for (let i = 0; i < newStr.length ; i++){
+        if(newStr[i]==newStr[j]){
+            if (i == parseInt(newStr.length/2)){
+                palin = true;
+                console.log(palin)
+                break;
+            }
+            j--;
+        }else{
+            break;
+        }
+    }
+return palin
+    
 };
 
 // Do not edit below this line
